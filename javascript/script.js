46dcaf2bc1a489267b7fca14fa4e3cfd0928c554
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-10-31 17:12:29 +0100

    header donde with js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,26 @@
 
-const menuBtnOpen = document.getElementById("menuBtnOpen")
-const menuBtnClose = document.getElementById("menuBtnClose")
+const menuBtn = document.getElementById("menuBtn")
+const headerUl = document.getElementById("headerUl")
+
+const xSrc = "./icons/close.svg"
+const menuSrc ="./icons/Menu.svg"
+
+let isOpen = false
+let isClosed = false
+
+const handleMenu = () =>{
+  if(isOpen){
+      menuBtn.src = menuSrc
+      isOpen = false
+      headerUl.style.display ="none"
+  }else{
+      menuBtn.src = xSrc
+      isOpen = true
+      headerUl.style.display ="flex"
+  }
+}
+
+const handleMenuOpen = ()=>{
+
+}
+menuBtn.addEventListener('click',handleMenu)
