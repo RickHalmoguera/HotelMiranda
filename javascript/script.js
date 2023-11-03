8596e204cc0b74b722f70d47fb8466545bd2c363
Ricardo Halmoguera <halmoguerar@gmail.com>
2023-11-03 17:30:43 +0100

    swiper

--- javascript/script.js
+++ javascript/script.js
@@ -20,4 +20,20 @@ const handleMenu = () =>{
   }
 }
 
+let swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop:true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints:{
+    1000:{
+      slidesPerView: 3
+    }
+  }
+});
+
+
 menuBtn.addEventListener('click',handleMenu)
